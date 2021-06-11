1fe12cdd14396e6744829fc11dbcb1542edab473
Leonardo Almeida Santana <61753754+leonardoarjs@users.noreply.github.com>
2021-06-11 14:04:15 -0300

    A blogueirinha

---
+++ me_segue_no_insta.js
@@ -0,0 +1,10 @@
+function solucao(largura, altura) {
+    if (largura === altura) {
+        console.log("QUADRADA")
+    } else if (largura > altura) {
+        console.log("PAISAGEM")
+    } else {
+        console.log("RETRATO")
+    }
+
+}
